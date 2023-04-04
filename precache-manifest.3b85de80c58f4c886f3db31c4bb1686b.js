29574e4436a12db4963ccbc063a10ec2718417d2
Dmitry Romansky <romansky.d@gmail.com>
2023-04-04 15:23:18 +0700

    Updates

---
+++ precache-manifest.3b85de80c58f4c886f3db31c4bb1686b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc854faa5c0321a05e2ec34eb1407bd9",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "1a12926b8a9444fc309a",
+    "url": "/weather/static/css/2.ff582875.chunk.css"
+  },
+  {
+    "revision": "0afb2bd45d9333c5bc4b",
+    "url": "/weather/static/css/main.67443a61.chunk.css"
+  },
+  {
+    "revision": "1a12926b8a9444fc309a",
+    "url": "/weather/static/js/2.f4742cb1.chunk.js"
+  },
+  {
+    "revision": "0afb2bd45d9333c5bc4b",
+    "url": "/weather/static/js/main.6312239a.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
